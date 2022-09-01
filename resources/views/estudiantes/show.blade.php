6d0c201a7470791efe0f58365048fd8408c8fce3
charlimora <ca.agudelo.2021@gmail.com>
2022-09-01 12:32:49 -0500

    Segunda consulta para mostrar datos de nacimiento

--- resources/views/estudiantes/show.blade.php
+++ resources/views/estudiantes/show.blade.php
@@ -4,13 +4,21 @@
 
 @section('contenido')
 <br>
-<h3>Ver detalles del Estudiante</h3>
+<h3 class="text-center">Ver detalles del Estudiante</h3>
 
-<p>{{$query1}}</p>
+<h3>Datos de expedición</h3>
 @foreach ($query1 as $consulta)
     <p>País de Expedición: {{$consulta->nomPais}}</p>
     <p>Departamento de expedición: {{$consulta->NomDepart}}</p>
     <p>Municipio de Expedición: {{$consulta->nomMuni}}</p>
+
+@endforeach
+
+<h3>Datos de nacimiento</h3>
+@foreach ($query2 as $consulta)
+    <p>País de Nacimiento: {{$consulta->nomPais}}</p>
+    <p>Departamento de Nacimiento: {{$consulta->NomDepart}}</p>
+    <p>Municipio de Nacimiento: {{$consulta->nomMuni}}</p>
 
 @endforeach
 
